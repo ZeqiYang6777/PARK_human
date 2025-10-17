f03466498daa95f23a9ca8ca05f6aaf3af4b0f4e
ZeqiYang6777 <1198983056@qq.com>
2025-10-17 10:42:58 +0800

    push

--- Assets/Amazing Assets/Curved World/Shaders/High Definition/Lit/LitConstantPass.hlsl
+++
@@ -1,9 +0,0 @@
-// Curved World <http://u3d.as/1W8h>
-// Copyright (c) Amazing Assets <https://amazingassets.world>
- 
-#ifndef SHADERPASS
-#error Undefine_SHADERPASS
-#endif
-
-// This include will define the various Attributes/Varyings structure
-#include "VaryingMesh.hlsl"
