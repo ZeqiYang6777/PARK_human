64948b7b668188531325a92a9c15917121b9c736
ZeqiYang6777 <1198983056@qq.com>
2025-11-19 13:25:58 +0800

    push

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+
+public class restart : MonoBehaviour
+{
+    public void OnlonginButtonClick()
+
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+
+public class StartGame : MonoBehaviour 
+{
+    public void OnlonginButtonClick()
+
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
